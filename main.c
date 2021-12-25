b19853e5b77893f09b3f8aa49a5efef977da4742
miswlf0 <miswlf0@hotmail.com>
2021-12-24 17:26:22 -0800

    update main

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char **agrv) {
-	printf("main: hello world\n");
+	printf("main: started...\n");
+	printf("main: hello world!\n"); /* miswlf0 */
+	printf("main: completed\n");
 	return 0;
 }
