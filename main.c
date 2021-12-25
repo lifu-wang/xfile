1f6b06ee5b3e0b2e9108d77bd272f73d3dd1b335
miswlf0 <miswlf0@hotmail.com>
2021-12-25 10:33:49 -0800

    add xmas check

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+
+void check_xmas(int month, int day) 
+{
+	if (month == 12 && day == 25) 
+		printf("Merry X'mas!!!\n");
+	else 
+		printf("%d/%d is not Xmas yet\n", month, day);
+	return;
+}
+
+
 int main(int argc, char **agrv) {
-	printf("main: started...\n");
-	printf("main: hello world!\n"); /* miswlf0 */
-	printf("main: completed\n");
+	printf("main(): Just Started...\n");
+	check_xmas(12, 10);
+	check_xmas(12, 25);
+	printf("Completed\n");
 	return 0;
 }
